3e1688e097de5d1514af25c7cc8f59e46f5caee7
Kayla Oates <koates7@gatech.edu>
2018-01-28 05:46:52 -0500

    Tried (and failed) to add sms functionality

--- app/src/main/java/com/example/emora/followme/NewWatcherScreen.java
+++ app/src/main/java/com/example/emora/followme/NewWatcherScreen.java
@@ -1,14 +1,25 @@
 package com.example.emora.followme;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NewWatcherScreen extends AppCompatActivity {
 
@@ -41,10 +52,22 @@ public class NewWatcherScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (confirmationAttempt()) {
+
                     Watcher watcher = new Watcher(etWatcherName.getText().toString(),
                             etWatcherNumber.getText().toString(), chRestrictNewWatcher.isChecked(),
-                            chSendNewWatcherRequest.isChecked(),null);
+                            chSendNewWatcherRequest.isChecked());
+                    sendText("+919124329083", "testing roger wilson");
+
+                } else {
+                    Toast.makeText(NewWatcherScreen.this, "You fucked up!", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        btnNewWatcherToWatchlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewWatcherScreen.this, WatchlistScreen.class));
             }
         });
 
@@ -56,5 +79,22 @@ public class NewWatcherScreen extends AppCompatActivity {
                 etWatcherNumber.getText().toString() != null;
 
     }
+    public void sendText(String watcherNum, String watcherCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.SEND_SMS},1);
+        ArrayList<String> messages = new ArrayList<>();
+        messages.add(watcherCode);
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
 
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+        SmsManager smsManager = SmsManager.getDefault();
+        if (watcherNum != null) {
+            smsManager.sendMultipartTextMessage(watcherNum,
+                    null, messages, null, null);
+        }
+    }
 }
+
+
+    }
